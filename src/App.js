b32be80758e6bbd7231392d9d76d4db5e8c212ee
César Gómez <caugustog89@gmail.com>
2024-06-29 12:12:38 -0500

    Se elimina header preestablesido del archivo app.js
    Se limpia archivo app.css

--- src/App.js
+++ src/App.js
@@ -4,22 +4,9 @@ import Header from './Components/Header/header.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HOLA MUNDO CON REACT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      
     </div>
   );
 }
