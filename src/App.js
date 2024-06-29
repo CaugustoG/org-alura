c7b449825c0dd36e930eae8c5c7dbaf272386789
César Gómez <caugustog89@gmail.com>
2024-06-29 10:51:20 -0500

    Se agrega imagen banner header, se crean carpetas component/Header, se crea archivo header.jsx

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src="/img/Header_total.png" alt = "Banner header inicial"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
