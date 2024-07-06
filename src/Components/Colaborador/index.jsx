7108692832cd988fd407a85448c8a1bacbd0dd29
César Gómez <caugustog89@gmail.com>
2024-07-05 21:21:39 -0500

    Se crean estilos y jsx al componente colaborador

--- src/Components/Colaborador/index.jsx
+++ src/Components/Colaborador/index.jsx
@@ -1,8 +1,14 @@
 import "./Colaborador.css"
 
 const Colaborador = () =>{
-    return <div>
-
+    return <div className="colaborador">
+        <div className="encabezado">
+            <img src="https://avatars.githubusercontent.com/u/85918673?v=" alt="foto perfil"/>
+        </div>
+        <div className="info">
+            <h4>César Gómez</h4>
+            <h5>Front end</h5>
+        </div>
     </div>
 }
 
--- src/Components/Equipo/index.jsx
+++ src/Components/Equipo/index.jsx
@@ -1,4 +1,5 @@
 import "./Equipo.css"
+import Colaborador from "../Colaborador"
 
 
 
@@ -15,7 +16,13 @@ const Equipo = (props) => {
     return <section className="equipo" style={obj}>
         <h3 style={estiloTitulo}>{titulo}</h3>
         <div className="colaboradores">
-
+            <Colaborador/>
+            <Colaborador/>
+            <Colaborador/>
+            <Colaborador/>
+            <Colaborador/>
+            <Colaborador/>
+            <Colaborador/>
         </div>
     </section>
 }
